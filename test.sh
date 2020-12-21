b8829e74855c9cc816e8333eeabe0f5685181d26
Oskar Persson <oskar.persson@polken.se>
2020-12-21 22:30:01 +0100

    Cleanup

--- test.sh
+++ test.sh
@@ -13,43 +13,24 @@ git checkout $DJANGO_VERSION
 pip install -r tests/requirements/py3.txt
 
 python tests/runtests.py --settings=testapp.settings --noinput --keepdb \
-    aggregation \
-    aggregation_regress \
-    annotations \
-    backends \
-    basic \
-    bulk_create \
-    constraints \
     custom_columns \
-    custom_lookups \
     custom_managers \
     custom_methods \
     custom_migration_operations \
     custom_pk \
-    datatypes \
     dates \
-    datetimes \
-    db_functions \
     db_typecasts \
     db_utils \
-    dbshell \
     defer \
-    defer_regress \
-    delete \
-    delete_regress \
     distinct_on_fields \
     empty \
-    expressions \
-    expressions_case \
     expressions_window \
     extra_regress \
     field_deconstruction \
     field_defaults \
     field_subclassing \
     filtered_relation \
-    fixtures \
     fixtures_model_package \
-    fixtures_regress \
     force_insert_update \
     foreign_object \
     from_db_value \
@@ -57,13 +38,7 @@ python tests/runtests.py --settings=testapp.settings --noinput --keepdb \
     generic_relations_regress \
     get_earliest_or_latest \
     get_object_or_404 \
-    get_or_create \
-    indexes \
-    inspectdb \
-    introspection \
-    invalid_models_tests \
     known_related_objects \
-    lookup \
     m2m_and_m2o \
     m2m_intermediary \
     m2m_multiple \
@@ -76,13 +51,6 @@ python tests/runtests.py --settings=testapp.settings --noinput --keepdb \
     many_to_many \
     many_to_one \
     max_lengths \
-    migrate_signals \
-    migration_test_data_persistence \
-    migrations \
-    migrations2 \
-    model_fields \
-    model_indexes \
-    model_options \
     mutually_referential \
     nested_foreign_keys \
     null_fk \
@@ -91,20 +59,14 @@ python tests/runtests.py --settings=testapp.settings --noinput --keepdb \
     one_to_one \
     or_lookups \
     order_with_respect_to \
-    ordering \
     pagination \
     prefetch_related \
-    queries \
     queryset_pickle \
     raw_query \
     reverse_lookup \
     save_delete_hooks \
-    schema \
-    select_for_update \
     select_related \
     select_related_onetoone \
     select_related_regress \
-    transaction_hooks \
-    transactions \
     update \
     update_only_fields
